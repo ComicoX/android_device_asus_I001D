98c79b32964f0286210a7a38c030cefd014a1281
ComicoTeam <comico_work@outlook.com>
2020-04-09 16:10:13 +0300

    I01WD: Force enable Vowifi and Volte

--- system_prop.mk
+++ system_prop.mk
@@ -100,6 +100,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
 
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.dbg.ims_volte_enable=1 \
+	persist.dbg.volte_avail_ovr=1 \
+	persist.dbg.vt_avail_ovr=1 \
+	persist.dbg.wfc_avail_ovr=1 \
+	persist.radio.rat_on=combine \
+	persist.radio.data_ltd_sys_ind=1 \
+	persist.radio.data_con_rprt=1 \
+	persist.radio.calls.on.ims=1
+
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=Comico 
